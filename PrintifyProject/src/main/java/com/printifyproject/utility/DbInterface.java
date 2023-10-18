530db5afec92e5df684c47999a33dd1f60e03614
Jason Strate <jasonstrate@gmail.com>
2023-10-17 20:04:32 -0500

    module-info adds

--- PrintifyProject/src/main/java/com/printifyproject/utility/DbInterface.java
+++ PrintifyProject/src/main/java/com/printifyproject/utility/DbInterface.java
@@ -5,24 +5,24 @@ import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 public class DbInterface {
-    private static final SessionFactory sessionFactory;
+   //private static final SessionFactory sessionFactory;
 
-    // Static initialization block to create the SessionFactory
-    static {
-        try {
-            // Load your Hibernate configuration (hibernate.cfg.xml or create a configuration programmatically)
-            Configuration configuration = new Configuration().configure();
+   //// Static initialization block to create the SessionFactory
+   //static {
+   //    try {
+   //        // Load your Hibernate configuration (hibernate.cfg.xml or create a configuration programmatically)
+   //        Configuration configuration = new Configuration().configure();
 
-            // Build the SessionFactory
-            sessionFactory = configuration.buildSessionFactory();
-        } catch (Exception ex) {
-            throw new ExceptionInInitializerError(ex);
-        }
-    }
+   //        // Build the SessionFactory
+   //        sessionFactory = configuration.buildSessionFactory();
+   //    } catch (Exception ex) {
+   //        throw new ExceptionInInitializerError(ex);
+   //    }
+   //}
 
-    // Method to obtain a new Hibernate session
-    public static Session openSession() {
-        return sessionFactory.openSession();
-    }
+   //// Method to obtain a new Hibernate session
+   //public static Session openSession() {
+   //    return sessionFactory.openSession();
+   //}
 }
 
--- PrintifyProject/src/main/java/module-info.java
+++ PrintifyProject/src/main/java/module-info.java
@@ -1,6 +1,16 @@
 module com.printifyproject.printifyproject {
     requires javafx.controls;
     requires javafx.fxml;
+    requires jakarta.persistence;
+    requires com.fasterxml.jackson.annotation;
+    requires com.fasterxml.jackson.core;
+    requires com.fasterxml.jackson.databind;
+    requires java.validation;
+    requires org.hibernate.orm.core;
+    requires org.slf4j;
+    requires okhttp3;
+    requires java.persistence;
+    requires annotations;
 
 
     opens com.printifyproject.printifyproject to javafx.fxml;
