f10d01ea0c92123ff8ca1726464d58d9e2fcc010
david-hang-mn <123279566+david-hang-mn@users.noreply.github.com>
2023-10-28 16:16:22 -0500

    Update module-info.java to run API calls
    Needed to add packages to jackson.databind in order to run API tests.

--- PrintifyProject/src/main/java/module-info.java
+++ PrintifyProject/src/main/java/module-info.java
@@ -11,7 +11,9 @@ module com.printifyproject.printifyproject {
     requires javafx.graphics;
     requires jakarta.validation;
 
-
+    opens com.printifyproject.printifyapi.model.catalog to com.fasterxml.jackson.databind;
+    opens com.printifyproject.printifyapi.model.product to com.fasterxml.jackson.databind;
+    opens com.printifyproject.printifyapi.model.shop to com.fasterxml.jackson.databind;
     opens com.printifyproject.printifyproject to javafx.fxml;
     exports com.printifyproject.printifyproject;
 }
