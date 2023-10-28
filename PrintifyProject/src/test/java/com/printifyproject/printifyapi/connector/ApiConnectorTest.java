007218c4c47cf1852d9c063800e25d133867ce93
david-hang-mn <123279566+david-hang-mn@users.noreply.github.com>
2023-10-28 16:06:23 -0500

    Update API Testing
    Added more ApiConnector testing and did some Catalog testing.

--- PrintifyProject/src/test/java/com/printifyproject/printifyapi/connector/ApiConnectorTest.java
+++ PrintifyProject/src/test/java/com/printifyproject/printifyapi/connector/ApiConnectorTest.java
@@ -2,6 +2,8 @@ package com.printifyproject.printifyapi.connector;
 
 import com.printifyproject.printifyapi.model.catalog.Blueprint;
 import com.printifyproject.printifyapi.model.catalog.PrintProvider;
+import com.printifyproject.printifyapi.model.product.Product;
+import com.printifyproject.printifyapi.model.product.ProductSet;
 import com.printifyproject.printifyapi.model.shop.Shop;
 import okhttp3.Request;
 import org.junit.jupiter.api.AfterEach;
@@ -28,17 +30,17 @@ class ApiConnectorTest {
 
     @Test
     void sendRequest() {
-        /* Test 1: Create request to Printify's base URL */
+        /* Send request to Printify's base URL */
         Request baseRequest = apiConnector.createRequest("");
         assertEquals(404, apiConnector.sendRequest(baseRequest).code());
 
-        /* Test 2: Create unauthenticated request to retrieve blueprints from catalog */
+        /* Send unauthenticated request to retrieve blueprints from catalog */
         Request unauthenticatedRequest = new Request.Builder()
                 .url(BASE_URL + BLUEPRINTS_URL)
                 .build();
         assertEquals(401, apiConnector.sendRequest(unauthenticatedRequest).code());
 
-        /* Test 3: Create request to retrieve blueprints from catalog */
+        /* Send request to retrieve blueprints from catalog */
         Request blueprintsRequest = apiConnector.createRequest(BLUEPRINTS_URL);
         assertEquals(200, apiConnector.sendRequest(blueprintsRequest).code());
     }
@@ -57,6 +59,8 @@ class ApiConnectorTest {
         //Create request for print providers
         Request printProvidersRequest = apiConnector.createRequest("catalog/print_providers.json");
         assertEquals(BASE_URL + "catalog/print_providers.json", printProvidersRequest.url().toString());
+        assertEquals(true, printProvidersRequest.isHttps());
+        assertEquals("GET", printProvidersRequest.method());
     }
 
     @Test
@@ -64,23 +68,25 @@ class ApiConnectorTest {
         //Get the Object shop
         Shop[] shops = apiConnector.getObject(SHOPS_URL, Shop[].class);
         Shop shop = shops[0];
-        System.out.println(shop.getShopId() + shop.getTitle() + shop.getSalesChannel());
+        assertEquals(11996530, shop.getShopId());
+        assertEquals("Capstone", shop.getTitle());
+        assertEquals("disconnected", shop.getSalesChannel());
 
-//        apiConnector.getObject(BLUEPRINTS_URL, PrintProvider[].class);
+        ProductSet productSet = apiConnector.getObject("shops/11996530/products.json", ProductSet.class);
+        assertEquals("/?page=1", productSet.getFirst_page_url());
     }
 
     @Test
     void getList() {
         List<Shop> shopList = apiConnector.getList(SHOPS_URL, Shop.class);
         Shop shop = shopList.get(0);
-
         assertEquals(11996530, shop.getShopId());
         assertEquals("Capstone", shop.getTitle());
         assertEquals("disconnected", shop.getSalesChannel());
 
         List<PrintProvider> printProviders = apiConnector.getList("catalog/print_providers.json", PrintProvider.class);
         assertEquals("AVMM", printProviders.get(0).getTitle());
-//        apiConnector.getList(BLUEPRINTS_URL, Blueprint.class);
+        assertEquals("LV", printProviders.get(0).getLocation().getCountry());
     }
 
     @AfterEach
