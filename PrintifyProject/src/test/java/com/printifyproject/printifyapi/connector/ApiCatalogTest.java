b26d166630cf52dc482209a991e79dc631448fde
david-hang-mn <123279566+david-hang-mn@users.noreply.github.com>
2023-10-28 23:55:03 -0500

    Update ApiCatalogTest.java
    Changed logger to log4j.

--- PrintifyProject/src/test/java/com/printifyproject/printifyapi/connector/ApiCatalogTest.java
+++ PrintifyProject/src/test/java/com/printifyproject/printifyapi/connector/ApiCatalogTest.java
@@ -1,11 +1,12 @@
 package com.printifyproject.printifyapi.connector;
 
-import com.printifyproject.printifyapi.model.catalog.Blueprint;
-import com.printifyproject.printifyapi.model.catalog.PrintProvider;
+import com.printifyproject.printifyapi.api.ApiCatalog;
+import com.printifyproject.printifyapi.catalog.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 
 import java.util.List;
 
@@ -16,17 +17,26 @@ class ApiCatalogTest {
 
     @BeforeEach
     void setUp() {
-        Logger logger = LoggerFactory.getLogger(ApiCatalog.class);
+        Logger logger = LogManager.getLogger();
         apiCatalog = new ApiCatalog(logger);
     }
 
     @Test
     void getBlueprint() {
+        Blueprint blueprintId5 = apiCatalog.getBlueprint(5);
+        assertEquals("Men's Cotton Crew Tee", blueprintId5.getTitle());
+        assertEquals("Next Level", blueprintId5.getBrand());
+        assertEquals("3600", blueprintId5.getModel());
+
+        //The following blueprint ids do not exist
+        assertEquals(null, apiCatalog.getBlueprint(0));
+        assertEquals(null, apiCatalog.getBlueprint(-1));
+        assertEquals(null, apiCatalog.getBlueprint(10000));
     }
 
     @Test
     void getBlueprints() {
-        System.out.println(apiCatalog.getBlueprints());
+//        System.out.println(apiCatalog.getBlueprints());
 
     }
 
@@ -37,12 +47,13 @@ class ApiCatalogTest {
         assertEquals("Dimona Tee", dimonaTee.getTitle());
         assertEquals("US", dimonaTee.getLocation().getCountry());
 
+        //Following print provider ids don't exist
         assertEquals(null, apiCatalog.getPrintProvider(0));
         assertEquals(null, apiCatalog.getPrintProvider(-1));
-        assertEquals(null, apiCatalog.getPrintProvider(1000));
-        System.out.println(apiCatalog.getPrintProvider(100).getLocation().getCountry());
+        assertEquals(null, apiCatalog.getPrintProvider(10000));
     }
 
+    //This is the method that takes no arguments and returns all print providers
     @Test
     void getPrintProviders() {
         List<PrintProvider> printProviderList = apiCatalog.getPrintProviders();
@@ -51,15 +62,43 @@ class ApiCatalogTest {
         assertEquals("LV", printProviderList.get(0).getLocation().getCountry());
     }
 
+    //This is the testGetPrintProviders() method that takes a blueprint id (int) as arg, to search for Print Providers
     @Test
     void testGetPrintProviders() {
+        List<PrintProvider> printProviderList = apiCatalog.getPrintProviders(5);
+        // There are 12 print providers for blueprint id = 5, (Men's Cotton Crew Tee)
+        assertEquals(12, printProviderList.size());
+
+        // The second print provider for Men's cotton tee is Dimona Tee
+        assertEquals(61, printProviderList.get(1).getPrintProviderKey());
+        assertEquals("Dimona Tee", printProviderList.get(1).getTitle());
+
+        //The following blueprint ids do not exist
+        assertEquals(null, apiCatalog.getPrintProviders(0));
+        assertEquals(null, apiCatalog.getPrintProviders(-1));
+        assertEquals(null, apiCatalog.getPrintProviders(10000));
     }
 
     @Test
     void getShipping() {
+        //Get shipping info from Print Provider: Dimona Tee, searching for Blueprint of Men Cotton Crew Tee
+        Shipping shipping = apiCatalog.getShipping(5, 61);
+
+        //Get the handling time from shipping
+        HandlingTime handlingTime = shipping.getHandlingTime();
+        assertEquals(10, handlingTime.getValue());
+        assertEquals("day", handlingTime.getUnit());
+
+        //Get the list of profiles
+        List<Profile> profileList = shipping.getProfiles();
+        assertEquals(4, profileList.size());
     }
 
     @Test
     void getVariants() {
+        VariantSet variantSet = apiCatalog.getVariants(5, 61);
+        assertEquals("Dimona Tee", variantSet.getTitle());
+        assertEquals(61, variantSet.getVariantKey());
+        assertEquals(18, variantSet.getVariants().size());  //Provider has 18 Variants
     }
 }
