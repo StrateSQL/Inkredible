b5db038795d892a5776fe92001dd852d58180047
Mauricio <fuentesmauricio75@gmail.com>
2023-11-10 20:14:01 -0600

    Colors can be added to list in product profiles list screen

--- PrintifyProject/src/main/java/com/printifyproject/printifyproject/ProductProfilesListScreenController.java
+++ PrintifyProject/src/main/java/com/printifyproject/printifyproject/ProductProfilesListScreenController.java
@@ -1,9 +1,7 @@
 package com.printifyproject.printifyproject;
 
-import com.printifyproject.orm.model.BlueprintEntity;
-import com.printifyproject.orm.model.PrintProviderEntity;
-import com.printifyproject.orm.service.BlueprintService;
-import com.printifyproject.orm.service.ServiceHelper;
+import com.printifyproject.orm.model.*;
+import com.printifyproject.orm.service.*;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -22,10 +20,12 @@ import java.awt.*;
 import java.io.IOException;
 import java.net.URL;
 import java.util.List;
+import java.util.Optional;
 import java.util.ResourceBundle;
+import java.util.Set;
 import java.util.stream.Collectors;
 
-import com.printifyproject.orm.service.PrintProviderService;
+import org.apache.logging.log4j.core.pattern.AbstractStyleNameConverter;
 
 public class ProductProfilesListScreenController implements Initializable {
 
@@ -40,30 +40,86 @@ public class ProductProfilesListScreenController implements Initializable {
     private ChoiceBox<String> printProviderChoiceBox;
     @FXML
     private ListView<String> colorsList;
+    @FXML
+    private ChoiceBox<String> colorsChoiceBox;
+    private BlueprintService blueprintService;
+    private BlueprintPrintProviderService blueprintPrintProviderService;
+    private ServiceHelper serviceHelper;
+
+    private BlueprintEntity selectedBlueprintEntity;
+    private PrintProviderEntity selectedPrintProviderEntity;
+    private BlueprintPrintProviderEntity selectedBlueprintPrintProviderEntity;
 
 
-    public void initialize(URL  arg0, ResourceBundle arg1){
+
+    public void initialize(URL arg0, ResourceBundle arg1) {
         ServiceHelper.initContext();
-        ServiceHelper serviceHelper = new ServiceHelper();
-        BlueprintService blueprintService = serviceHelper.getBlueprintService();
+        serviceHelper = new ServiceHelper();
+        blueprintService = serviceHelper.getBlueprintService();
         List<BlueprintEntity> results = blueprintService.findAll();
 
         List<String> blueprintTitles = getTitles(results);
 
 
 //        Initialization code goes here
-//        myChoiceBox.getItems().addAll(names);
+        productBlueprintChoiceBox.getItems().addAll(blueprintTitles);
+        productBlueprintChoiceBox.setOnAction(this::onSelectedBlueprint);
+        printProviderChoiceBox.setOnAction(this::onSelectedPrintProvider);
     }
 
-    public List<String> getTitles(List<BlueprintEntity> list){
+    public List<String> getTitles(List<BlueprintEntity> list) {
 
         return list.stream()
-                    .map(BlueprintEntity::getTitle)
-                    .toList();
+                .map(BlueprintEntity::getTitle)
+                .toList();
+    }
+
+    public void onSelectedBlueprint(ActionEvent event){
+
+
+        Optional<BlueprintEntity> selectedBlueprint = blueprintService.getBlueprintByTitle(productBlueprintChoiceBox.getValue());
+
+
+        blueprintPrintProviderService = serviceHelper.getBlueprintPrintProviderService();
+
+
+        selectedBlueprint.ifPresent(
+                blueprintEntity -> {
+                    List<String> printProviderNames = blueprintPrintProviderService.getPrintProviderNames(blueprintEntity.getBlueprintId());
+                    printProviderChoiceBox.getItems().addAll(printProviderNames);
+                    selectedBlueprintEntity = blueprintEntity;
+                }
+        );
 
     }
 
-    public void testButton(ActionEvent event){
+    public void onSelectedPrintProvider(ActionEvent event){
+        PrintProviderService printProviderService = serviceHelper.getPrintProviderService();
+        Optional<PrintProviderEntity> selectedPrintProvider = printProviderService.getPrintProviderByName(printProviderChoiceBox.getValue());
+
+        selectedPrintProvider.ifPresent(printProviderEntity -> {
+            selectedPrintProviderEntity = printProviderEntity;
+            Optional<BlueprintPrintProviderEntity> blueprintPrintProviderEntity =  blueprintPrintProviderService.findByKeys(selectedBlueprintEntity,selectedPrintProviderEntity);
+            blueprintPrintProviderEntity.ifPresent(blueprintPrintProviderEntity1 -> {
+                ColorService colorService = serviceHelper.getColorService();
+                Set<String> colorEntitySet =  colorService.getColorsByBlueprintPrintProviderId(blueprintPrintProviderEntity1.getId());
+                colorsChoiceBox.getItems().addAll(colorEntitySet);
+                selectedBlueprintPrintProviderEntity = blueprintPrintProviderEntity1;
+            });
+
+        });
+
+
+
+    }
+
+    public void addColorToColorList(ActionEvent event){
+        String color = colorsChoiceBox.getValue();
+        colorsList.getItems().add(color);
+    }
+
+
+    public void testButton(ActionEvent event) {
         ServiceHelper.initContext();
         ServiceHelper serviceHelper = new ServiceHelper();
         PrintProviderService blueprintService = serviceHelper.getPrintProviderService();
@@ -80,5 +136,13 @@ public class ProductProfilesListScreenController implements Initializable {
         stage.setScene(scene);
         stage.show();
     }
+
+    public void saveProductProfile(){
+        PrintSpecService printSpecService = serviceHelper.getPrintSpecService();
+        PrintSpecEntity printSpecEntity= new PrintSpecEntity();
+        printSpecEntity.setBlueprintPrintProvider(selectedBlueprintPrintProviderEntity);
+        printSpecEntity.set
+    }
+
 
 }
