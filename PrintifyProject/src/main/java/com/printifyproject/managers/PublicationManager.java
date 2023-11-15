2e228d9bd6e2d7af4b8b84e6e42776cf10fec696
Jason Strate <jasonstrate@gmail.com>
2023-11-14 21:56:07 -0600

    Update PublicationManager.java

--- PrintifyProject/src/main/java/com/printifyproject/managers/PublicationManager.java
+++ PrintifyProject/src/main/java/com/printifyproject/managers/PublicationManager.java
@@ -8,7 +8,7 @@ import java.util.Optional;
 import java.util.UUID;
 
 public class PublicationManager {
-    public boolean UploadPrductToPrintify (int productId) {
+    public boolean UploadProductToPrintify (int productId) {
         ServiceHelper.initContext();
         ServiceHelper serviceHelper = new ServiceHelper();
         ProductService productService = serviceHelper.getProductService();
